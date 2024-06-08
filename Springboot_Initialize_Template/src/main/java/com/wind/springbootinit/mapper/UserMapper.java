0f28f25f43c6e46d6029e34e3a0920dae87d66d5
wangjiayu <1303183851@qq.com>
2024-06-08 21:37:19 +0800

    init

---
+++ Springboot_Initialize_Template/src/main/java/com/wind/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wind.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wind.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
